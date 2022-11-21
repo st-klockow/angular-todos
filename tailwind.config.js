554c5c5308c17ba568d216d828b7098a0613a0b4
Steve Klockow <s.klockow@heinlein-video.de>
2022-11-21 14:08:07 +0100

    add tailwind css

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  purge: {
+    content: ["./src/**/*.{html,ts}"],
+  },
+  content: [],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
